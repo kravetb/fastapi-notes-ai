67a5b134bf4365b991e88ecff2cc4e733d5a5962
kravetb <kravetsbodj@gmail.com>
2025-03-15 12:31:49 +0200

    Created a project structure & added project containerization & set up local development

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from app.config import get_settings
+
+settings = get_settings()
+
+engine = create_async_engine(settings.SQLALCHEMY_DB_URL, echo=False)
+
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+
+async def get_db() -> AsyncSession:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
